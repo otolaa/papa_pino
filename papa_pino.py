9f343fc814872f86b70e02f1fa21f3126fcd5055
otolaa <kalabunga82@gmail.com>
2024-02-27 23:51:04 +0200

    🍕

--- papa_pino.py
+++ papa_pino.py
@@ -1,7 +1,5 @@
 """ https://xn--80aayufbbb.xn--p1ai/ """
 import sys, time, json
-from datetime import datetime
-import requests, fake_useragent
 from bs4 import BeautifulSoup
 from tinydb import TinyDB, Query
 from selenium import webdriver
@@ -31,6 +29,20 @@ def load_json(path):
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
+def get_pagen(browser, *args):
+    try:
+        pagen = browser.find_element(By.CSS_SELECTOR, 'div.ajax-pager-wrap')
+        if pagen is not None:
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            return True
+        else:
+            return False
+
+    except Exception as e:
+        print(sys.exc_info()[1])
+        return False
+
 def get_selenium(url, pg = None):
     """ start selenium """       
     service = Service(executable_path='/usr/local/bin/geckodriver')
@@ -54,23 +66,34 @@ def get_selenium(url, pg = None):
 
     driver.maximize_window()
     driver.get(url)
-    time.sleep(7)
+    time.sleep(3)
     
     try:
         ''' Этот код будет ждать 10 секунд до того, как отдаст исключение TimeoutException или если найдет элемент за эти 10 секунд, то вернет его. 
         WebDriverWait по умолчанию вызывает ExpectedCondition каждые 500 миллисекунд до тех пор, пока не получит успешный return. 
         Успешный return для ExpectedCondition имеет тип Boolean и возвращает значение true, либо возвращает not null для всех других ExpectedCondition типов. '''
-        element = WebDriverWait(driver, 20).until(
+        element = WebDriverWait(driver, 50).until(
             EC.presence_of_element_located((By.ID, "gift-modal"))
         )
 
-        html = driver.page_source        
+        # div.ajax-pager-wrap
+        if get_pagen(driver) is True:
+            print(f'\033[32m[+] is set pagen\033[0m', end='\n')
+
+        while True:
+            if get_pagen(driver) is True:
+                print(f'\033[32m[+] is set pagen\033[0m', end='\n')
+                time.sleep(1)
+            else:
+                break
+
+        html = driver.page_source
         if pg is not None:
             wtf(html, pg)
 
         print(f'\033[32m[+] success return html \033[0m', end='\n')
         return html
-    
+
     except Exception as e:
         print(f'\033[31m[-] errorf: {sys.exc_info()[1]}\033[0m', end='\n')
 
@@ -91,23 +114,66 @@ def get_links(html):
     
     return r
 
-def get_item(html):
+def get_items(html):
+    ''' '''
     soup = BeautifulSoup(html, 'lxml')
     r_b = soup.find('body', {})
+    items = r_b.find('div', {"class":'row product-list'})
 
-    for item in r_b.find_all('div', {"class":'row product-list'}):
-        # p(item.text.strip())
-        pass
+    res = []
+    for item in items.find_all('div', {"class":'col-lg-3 col-xl-3 col-sm-6 product-ajax-cont'}):
+        p = item.find('div', {'class':'product-info'})
+        f = item.find('div', {'class':'product-footer'})
+        img = item.find('a', {'itemprop':'image'})
+        
+        if p is None:
+           continue
+
+        p_t = p.find('a', {'class':'product-title'})
+        p_d = p.find('div', {'class':'product-description'})
+        p_prise = f.find('span', {'itemprop':'price'})
+        p_m = f.find('span', {'class':'weight'})
+
+        r = {}
+        r['name'] = p_t.text.strip()
+        r['url'] = p_t.get('href')
+        if p_d is not None:
+            r['description'] = p_d.text.strip()
+
+        if p_prise is not None:
+            r['prise'] = p_prise.text.strip()
+
+        if p_m is not None:
+            rw = []
+            for w in p_m.find_all('span'):
+                rw.append(w.text.strip())
+
+            r['weight'] = rw
+
+        if img is not None:
+            r['img'] = img.get('href')
+
+        res.append(r)
+
+    return res
 
 if __name__ == "__main__":
     ''' '''
-    url = 'https://xn--80aayufbbb.xn--p1ai/'
-    html = get_selenium(url, '_0_')
-    r = get_links(html)
+    url = 'https://xn--80aayufbbb.xn--p1ai'
+    # html = get_selenium(url, '_0_')
+    # r = get_links(html)
 
-    write_json(r, './json/link.json')
-    p(r)
+    # write_json(r, './json/link.json')
+    # p(r)
 
-    # url_1 = 'https://xn--80aayufbbb.xn--p1ai/catalog/pizza/'
-    # html_1 = get_selenium(url_1, '_pizza_')
-    # get_item(html_1)
+    #--------------------------------------------------------------#
+    pg = '_pizza_'
+    url_1 = '/catalog/pizza/'
+    html_1 = get_selenium(f'{url}{url_1}', '_pizza_')
+        
+    # html_1 = lf(f'./html/html{pg}.txt')
+
+    data = get_items(html=html_1)
+    write_json(data, f'./json/data{pg}.json')
+
+    print(f'\033[32m[+] parse: {len(data)} element \033[0m', end='\n')
