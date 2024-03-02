ba4f7acd0359c60e4dac9e52c6574237f9627534
otolaa <kalabunga82@gmail.com>
2024-03-02 12:06:49 +0200

    🍳

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-## small parsing https://xn--80aayufbbb.xn--p1ai/
+## parse https://xn--80aayufbbb.xn--p1ai/
 
-0. bypass Cloudflare block on serlenium
+bypass Cloudflare block on serlenium!?
+
+```
+$ python3 papa_pino.py
+```
 
 
--- papa_pino.py
+++ papa_pino.py
@@ -43,8 +43,8 @@ def get_pagen(browser, *args):
         print(sys.exc_info()[1])
         return False
 
-def get_selenium(url, pg = None):
-    """ start selenium """       
+def get_selenium_driver():
+    """ start selenium driver """
     service = Service(executable_path='/usr/local/bin/geckodriver')
     options = Options()
     options.add_experimental_option("detach", True)
@@ -65,8 +65,12 @@ def get_selenium(url, pg = None):
     })
 
     driver.maximize_window()
+    return driver
+
+def get_html(driver, url, pg = None):
+    """"""   
     driver.get(url)
-    time.sleep(3)
+    time.sleep(0.5)
     
     try:
         ''' Этот код будет ждать 10 секунд до того, как отдаст исключение TimeoutException или если найдет элемент за эти 10 секунд, то вернет его. 
@@ -114,6 +118,42 @@ def get_links(html):
     
     return r
 
+def get_properties(property_item):
+    pro = []
+    for prop_div in property_item.find_all('div', {"class":"row"}):
+        prop_t = prop_div.find('div', {"class":"option_title"})
+
+        if prop_t is not None:
+            # Дополнительно:
+            prop_title = prop_t.text.strip()
+            if prop_title == 'Дополнительно:':
+                continue
+            
+            prop_l = prop_div.find_all('span', {})
+            
+            
+            lb = {}
+            lb['title'] = prop_t.text.strip()
+            lb['label'] = [ls.text.strip() for ls in prop_l if len(ls.text.strip()) > 0]
+            pro.append(lb)
+    
+    sr = []
+    for pro_div in property_item.find_all('div', {"class":"row"}):
+        prop_t = pro_div.find('div', {"class":"option_title"})
+
+        if prop_t is None:
+            for spn in pro_div.find_all('span', {}):
+                span_t = spn.text.strip()
+                if len(span_t) > 0:
+                    sr.append(span_t)
+
+
+    
+    if len(sr) > 0:
+        pro.append({'title':'Дополнительно:','label':sr})
+
+    return pro
+
 def get_items(html):
     ''' '''
     soup = BeautifulSoup(html, 'lxml')
@@ -122,9 +162,15 @@ def get_items(html):
 
     res = []
     for item in items.find_all('div', {"class":'col-lg-3 col-xl-3 col-sm-6 product-ajax-cont'}):
-        p = item.find('div', {'class':'product-info'})
-        f = item.find('div', {'class':'product-footer'})
-        img = item.find('a', {'itemprop':'image'})
+        ''''''
+        product = item.find('div', {'class':'product-item'})
+        if product is None:
+            continue
+
+        property_item = product.find('div', {'class':'product-options'})
+        p = product.find('div', {'class':'product-info'})
+        f = product.find('div', {'class':'product-footer'})
+        img = product.find('a', {'itemprop':'image'})
         
         if p is None:
            continue
@@ -135,6 +181,8 @@ def get_items(html):
         p_m = f.find('span', {'class':'weight'})
 
         r = {}
+        r['pid'] = product.get('data-id')
+        r['unit'] = product.get('data-unit')
         r['name'] = p_t.text.strip()
         r['url'] = p_t.get('href')
         if p_d is not None:
@@ -152,6 +200,10 @@ def get_items(html):
 
         if img is not None:
             r['img'] = img.get('href')
+        
+        if property_item is not None:
+            ''' properties '''          
+            r['properties'] = get_properties(property_item)
 
         res.append(r)
 
@@ -160,20 +212,23 @@ def get_items(html):
 if __name__ == "__main__":
     ''' '''
     url = 'https://xn--80aayufbbb.xn--p1ai'
-    # html = get_selenium(url, '_0_')
-    # r = get_links(html)
+    
+    driver = get_selenium_driver();
+    get_html(driver, f'{url}', None)
 
-    # write_json(r, './json/link.json')
-    # p(r)
+    for href_ in load_json('./json/link.json'):
+        url_path = href_['href']
+        code_page = url_path.replace('/catalog/','').replace('/','').replace('-','_')
+        #--------------------------$$$---------------------------------#
+        print(f'\033[32m[+] start {url_path} code {code_page} \033[0m', end='\n')
 
-    #--------------------------------------------------------------#
-    pg = '_pizza_'
-    url_1 = '/catalog/pizza/'
-    html_1 = get_selenium(f'{url}{url_1}', '_pizza_')
-        
-    # html_1 = lf(f'./html/html{pg}.txt')
+        #--------------------------$$$---------------------------------#
+        pg = f'_{code_page}_'        
+        html_ = get_html(driver, f'{url}{url_path}', pg)
+            
+        # html_ = lf(f'./html/html{pg}.txt') # htm for file
 
-    data = get_items(html=html_1)
-    write_json(data, f'./json/data{pg}.json')
+        data = get_items(html=html_)
+        write_json(data, f'./json/data{pg}.json')
 
-    print(f'\033[32m[+] parse: {len(data)} element \033[0m', end='\n')
+        print(f'\033[32m[+] parse: {len(data)} element \033[0m', end='\n')
