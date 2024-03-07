f044d0398bb0dd08b782c27bf2c1e3d138ca04c4
otolaa <kalabunga82@gmail.com>
2024-03-07 19:38:30 +0200

    🍭

--- get_xml.py
+++ get_xml.py
@@ -1,7 +1,44 @@
 from lxml import etree, objectify
-import datetime, json, sys
+import datetime, json, sys, re
 from help import *
 
+
+def get_modifiers_groups():
+    '''*-|-@-@-|-*'''
+    m_groups = []
+    m_modifiers = []
+
+    for int, cat_item in enumerate(load_json('./json/link.json'), 1):
+        code_page = cat_item['href'].replace('/catalog/','').replace('/','').replace('-','_')
+
+        for offer in load_json(f'./json/data_{code_page}_.json'):
+            if len(offer['properties']) > 0:                
+                for key, prop in enumerate(offer['properties'], 0):
+                    if key == 0:
+                        continue
+
+                    m_item = {}
+                    m_item['gid'] = f"{int}{key}"
+                    m_item['name'] = f"{cat_item['name']} {prop['title'].replace(':','').lower()}"
+                    type_elem = prop['labels'][0]['type']
+                    m_item['type'] = get_type_m(type_elem) # one_one || all_one
+                    m_item['minimum'] = '1'
+                    m_item['maximum'] = get_maximun_m(type_elem)
+                
+                    if m_item['gid'] not in get_list_gid(m_groups):                        
+                        m_groups.append(m_item)
+
+                        #--@@--#
+                        for k_, l_ in enumerate(prop['labels'], 0):
+                            m_modifier = {}
+                            m_modifier['gid'] = m_item['gid']
+                            m_modifier['name'] = l_['title']
+                            m_modifier['price'] = l_['price']
+                            m_modifier['mid'] = f"{m_item['gid']}{k_}"
+
+                            m_modifiers.append(m_modifier)
+
+    return {'groups':m_groups, 'modifiers':m_modifiers}
 
 def get_doc():
     ''' create the root element '''
@@ -26,6 +63,23 @@ def get_doc():
     categoryElement = etree.SubElement(pageElementShop, 'categories')
     for int, category_item in enumerate(load_json('./json/link.json'), 1):
         etree.SubElement(categoryElement, 'category', id=str(int)).text = category_item['name']
+    
+    modifiersGroups = etree.SubElement(pageElementShop, 'modifiersGroups')
+    gg = get_modifiers_groups()
+    for m_group in gg['groups']:
+        modifiersGroup = etree.SubElement(modifiersGroups, 'modifiersGroup', id = m_group['gid'])
+        etree.SubElement(modifiersGroup, 'name').text = m_group['name']
+        etree.SubElement(modifiersGroup, 'type').text = m_group['type']
+        etree.SubElement(modifiersGroup, 'minimum').text = m_group['minimum']
+        etree.SubElement(modifiersGroup, 'maximum').text = m_group['maximum']
+    
+    modifierList = etree.SubElement(pageElementShop, 'modifiers')
+    for m_modifier in gg['modifiers']:
+        # p(m_modifier)
+        modifierElem = etree.SubElement(modifierList, 'modifier', id=str(m_modifier['mid']))
+        etree.SubElement(modifierElem, 'name').text = m_modifier['name']
+        etree.SubElement(modifierElem, 'price').text = f"+{m_modifier['price']}"
+        etree.SubElement(modifierElem, 'modifiersGroupId').text = m_modifier['gid']
 
     offersElement = etree.SubElement(pageElementShop, 'offers')
     for int, cat_item in enumerate(load_json('./json/link.json'), 1):
@@ -41,16 +95,34 @@ def get_doc():
             etree.SubElement(offerElem, 'picture').text = f"https://xn--80aayufbbb.xn--p1ai{offer['url']}"
             etree.SubElement(offerElem, 'categoryId').text = str(int)
 
+            if len(offer['properties']) > 0:
+                for k_, p_ in enumerate(offer['properties'], 0):
+                    if k_ == 0:
+                        continue
+                    '''@@'''
+                    mGroupsIds = etree.SubElement(offerElem, 'modifiersGroupsIds')
+                    etree.SubElement(mGroupsIds, 'modifiersGroupId').text = f"{int}{k_}"
+
             propsParameters = etree.SubElement(offerElem, 'parameters')
             if len(offer['properties']) > 0:
                 '''_@_@_'''
-                for prop in offer['properties']:
-                    propParameter = etree.SubElement(propsParameters, 'parameter', id=str(get_id()))
-                    etree.SubElement(propParameter, 'price').text = prop['prise']
-                    etree.SubElement(propParameter, 'description').text = prop['title']
+                for key, prop in enumerate(offer['properties'], 1):
+                    p_title =  prop['title']
+                    if p_title == 'Дополнительно:' or key > 1:
+                        continue
 
-                    unit = offer['unit'] if len(offer['unit']) > 0 else 'шт.'
-                    etree.SubElement(propParameter, 'descriptionIndex').text = str(get_description_index(unit))
+                    for p_label in prop['labels']:
+                        propParameter = etree.SubElement(propsParameters, 'parameter', id=str(get_id()))
+                        etree.SubElement(propParameter, 'price').text = p_label['price']
+                        etree.SubElement(propParameter, 'description').text = p_label['title']
+                        
+                        unit = offer['unit'] if len(offer['unit']) > 0 else 'шт'
+                        unit_text = re.sub(r"\d+", "", p_label['title']).replace('.','').strip()
+                        '''_@_@_@_'''
+                        val_unit = get_description_index(unit_text)
+                        unit = val_unit if len(val_unit) > 0 else get_description_index(unit)
+
+                        etree.SubElement(propParameter, 'descriptionIndex').text = unit
                 
             else:
                 propParameter = etree.SubElement(propsParameters, 'parameter', id=str(get_id()))
@@ -62,13 +134,13 @@ def get_doc():
                     if offer['weight'] and len(offer['weight']) > 0 and len(offer['weight'][0]) > 0:
                         etree.SubElement(propParameter, 'description').text = str(offer['weight'][0])
                     
-                    unit = offer['unit'] if len(offer['unit']) > 0 else 'шт.'
-                    etree.SubElement(propParameter, 'descriptionIndex').text = str(get_description_index(unit))
+                    unit = offer['unit'] if len(offer['unit']) > 0 else 'шт'
+                    etree.SubElement(propParameter, 'descriptionIndex').text = get_description_index(unit)
                 
                 except Exception as e:
-                    p(offer['name'], offer['unit'], sys.exc_info()[1])
+                    pcolor(f"{offer['name']} / {offer['unit']} / {sys.exc_info()[1]}", 1)
         
-        print(f'\033[32m[+] {int} add: {code_page}\033[0m', end='\n')
+        pcolor(f'[+] {int} add: {code_page}')
 
 
     return doc
--- help.py
+++ help.py
@@ -4,6 +4,9 @@ import random
 def p(text, *args):
     print(text, *args, sep=' / ', end='\n')
 
+def pcolor(text_code, color_num = 2):
+    print(f'\033[3{color_num}m{text_code}\033[0m', end='\n')
+
 def wtf(html, pg):
     with open(f'./html/html{pg}.txt', "w", encoding='utf8') as f:
         f.write(html)
@@ -20,24 +23,46 @@ def load_json(path):
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-def get_description_index(unit):
+def get_description_index(unit_code):
     ''' return index '''
-    
+    unit = unit_code.replace('.', '')
+
     nids = {
-        'ед.':0, 
-        'гр.':1, 'гр':1, 
-        'кг.':2, 
-        'мл.':3, 
-        'л.':4, 'л':4,
-        'см.':5, 'см':5,
-        'м.':6,
-        'мин.':7,
-        'ч.':8,
-        'шт.':9, 'штук.':9,
-        'порц.':10,
+        'ед':0,
+        'гр':1, 
+        'кг':2,
+        'мл':3,
+        'л':4, 'литра':4,
+        'см':5,
+        'м':6,
+        'мин':7,
+        'ч':8,
+        'шт':9, 'штук':9,
+        'порц':10,
     }
 
-    return nids[unit] if nids[unit] else ''
+    return (str(nids[unit]) if unit in nids.keys() else '')
 
 def get_id():
     return random.randrange(1992, 3999)
+
+def get_maximun_m(t):
+    if t == 'radio':
+        return str(1)
+    
+    if t == 'checkbox':
+        return str(10)
+
+def get_type_m(t):
+    if t == 'radio':
+        return 'one_one'
+
+    if t == 'checkbox':
+        return 'all_one'
+
+def get_list_gid(m_groups):
+    g_arr = []
+    for g_ in m_groups:
+        g_arr.append(g_['gid'])
+    
+    return set(g_arr)
